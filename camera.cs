776c7f3bb67e1393a42aabca9d415a82b436c471
Boaz Roskam <b.w.j.roskam.students@uu.nl>
2025-05-13 12:43:52 +0200

    Classes: Camera, primitive, intersection

---
+++ camera.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+
+public class Camera
+{
+	public Vector3 position;
+    public Vector3 lookAtDirection;
+    public Vector3 upDirection;
+    public ScreenPlane screenPlane;
+
+    public Camera() {
+        this.position = new Vector3(0, 0, 0);
+        this.lookAtDirection = new Vector3(0, 0, 1);
+        this.upDirection = new Vector3(0, 1, 0);
+        this.screenPlane = new ScreenPlane(new Vector3(10, 0, 0), new Vector3(-10, 0, 0), new Vector3(0, 0, 10), new Vector3(0, 0, -10));
+    }
+
+	public Camera(Vector3 position, Vector3 lookAtDirection, Vector3 upDirection, ScreenPlane screenPlane) {
+        this.position = position;
+        this.lookAtDirection = lookAtDirection;
+        this.upDirection = upDirection;
+        this.screenPlane = screenPlane;
+    }
+}
+
+public class ScreenPlane {
+    public Vector3 a;
+    public Vector3 b;
+    public Vector3 c;
+    public Vector3 d;
+
+    public ScreenPlane(Vector3 a, Vector3 b, Vector3 c, Vector3 d){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+}
---
+++ primitive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+using Template;
+
+public class Primitive
+{
+	public Color3 color;
+	public Primitive()
+	{
+
+	}
+}
+
+public class Sphere : Primitive {
+	public Vector3 position;
+	public float radius;
+
+	public Sphere(Vector3 position, int radius) {
+        this.position = position;
+        this.radius = radius;
+    }
+}
+
+public class Plane : Primitive {
+	public Vector3 normal;
+	public float distance;
+
+	public Plane(Vector3 normal, float distance) {
+		this.normal = normal;
+		this.distance = distance;
+	}
+}
+
+public class Intersection{
+	public float distance;
+	public Primitive? primitive;
+	public Vector3 normal;
+}
