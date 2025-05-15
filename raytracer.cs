a49637eabe97403415cc9a28e081c7eae2de49fb
Rein Spaan <reinspaan@gmail.com>
2025-05-15 15:56:37 +0200

    flipped debugger

--- raytracer.cs
+++ raytracer.cs
@@ -18,7 +18,7 @@ public class Raytracer {
 
 	public void Render() {
 		int debuggerOffsetX = ((3 * screen.width) / 4);
-		int debuggerOffsetY = (screen.height / 4);
+		int debuggerOffsetY = ((7*screen.height) / 8);
 
 		for (int row = (int)camera.screenPlane.downLeft.Y; row < (int)camera.screenPlane.upLeft.Y; row++) {
 			for (int col = (int)camera.screenPlane.upLeft.X; col < (int)camera.screenPlane.upRight.X; col++) {
@@ -42,22 +42,22 @@ public class Raytracer {
 
 				//Debugger
 				if (row == (camera.screenPlane.downLeft.Y + camera.screenPlane.upLeft.Y) / 2) {
-					screen.Plot((int)camera.position.X + debuggerOffsetX + 1, (int)camera.position.Y + debuggerOffsetY + 1, new Color3(0.0f, 0.5f, 0.5f));
-					screen.Plot((int)camera.position.X + debuggerOffsetX, (int)camera.position.Y + debuggerOffsetY + 1, new Color3(0.0f, 0.5f, 0.5f));
-					screen.Plot((int)camera.position.X + debuggerOffsetX + 1, (int)camera.position.Y + debuggerOffsetY, new Color3(0.0f, 0.5f, 0.5f));
-					screen.Plot((int)camera.position.X + debuggerOffsetX, (int)camera.position.Y + debuggerOffsetY, new Color3(0.0f, 0.5f, 0.5f));
+					screen.Plot((int)camera.position.X + debuggerOffsetX + 1, (int)-camera.position.Z + debuggerOffsetY + 1, new Color3(0.0f, 0.5f, 0.5f));
+					screen.Plot((int)camera.position.X + debuggerOffsetX, (int)-camera.position.Z + debuggerOffsetY + 1, new Color3(0.0f, 0.5f, 0.5f));
+					screen.Plot((int)camera.position.X + debuggerOffsetX + 1, (int)-camera.position.Z + debuggerOffsetY, new Color3(0.0f, 0.5f, 0.5f));
+					screen.Plot((int)camera.position.X + debuggerOffsetX, (int)-camera.position.Z + debuggerOffsetY, new Color3(0.0f, 0.5f, 0.5f));
 
 
 					if (col % 10 == 0) {
 						if (closestIntersection != null) {
-							screen.Line((int)(ray.orgin.X + debuggerOffsetX), (int)(ray.orgin.Z + debuggerOffsetY), (int)(closestIntersection.position.X + debuggerOffsetX), (int)(closestIntersection.position.Y + debuggerOffsetY), new Color3(1f, 0f, 0f));
+							screen.Line((int)(ray.orgin.X + debuggerOffsetX), (int)(-ray.orgin.Z + debuggerOffsetY), (int)(closestIntersection.position.X + debuggerOffsetX), (int)(-closestIntersection.position.Z + debuggerOffsetY), new Color3(1f, 0f, 0f));
 						} else {
-							screen.Line((int)(ray.orgin.X + debuggerOffsetX), (int)(ray.orgin.Z + debuggerOffsetY), (int)(ray.directionVector.X * 100 + debuggerOffsetX), (int)(ray.directionVector.Z * 100 + debuggerOffsetY), new Color3(1f, 0f, 0f));
+							screen.Line((int)(ray.orgin.X + debuggerOffsetX), (int)(-ray.orgin.Z + debuggerOffsetY), (int)(ray.directionVector.X * 100 + debuggerOffsetX), (int)(-ray.directionVector.Z * 100 + debuggerOffsetY), new Color3(1f, 0f, 0f));
 						}
 					}
 
 					foreach (Intersection intersection in intersections) {
-						screen.Plot((int)(intersection.position.X + debuggerOffsetX), (int)(intersection.position.Z + debuggerOffsetY), new Color3(0, 0, 1));
+						screen.Plot((int)(intersection.position.X + debuggerOffsetX), (int)(-intersection.position.Z + debuggerOffsetY), new Color3(0, 0, 1));
 					}
 				}
 			}
